9bea6133cf541e3e3b3549de8ff1fbc7050758ce
SUB0PT1MAL <92884665+SUB0PT1MAL@users.noreply.github.com>
2025-09-27 15:34:42 +0200

    Update Valley4Server_re.py

--- Valley4Server_re.py
+++ Valley4Server_re.py
@@ -329,7 +329,7 @@ async def download_track(ctx, info, guild_id, connection):
                 'paths': {'home': f'./dl/{guild_id}'},
                 'outtmpl': '%(id)s.%(ext)s'
             }) as ydl:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 with download_locks[guild_id]:
                     return await asyncio.get_event_loop().run_in_executor(
                         None, ydl.download, [webpage_url]
